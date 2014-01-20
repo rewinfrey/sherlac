e4deb302c7ded38dcbd1d175641b07786f51df26
Rick Winfrey <rick.winfrey@gmail.com>
2014-01-19 19:59:53 -0600

    Updates query-craig.category as setup for future changes

--- spec/clj/sherlac/query_craig/category_spec.clj
+++ spec/clj/sherlac/query_craig/category_spec.clj
@@ -3,29 +3,5 @@
             [sherlac.query-craig.category :refer :all]))
 
 (describe "find-category"
-  (it "returns a map of categories and their associated CL code"
-    (should= {:housing {:sub-categories {:apartments-housing "apa"
-                                         :rooms-shared       "roo"
-                                         :sublets-temporary  "sub"
-                                         :housing-wanted     "hsw"
-                                         :vacation-rentals   "vac"
-                                         :parking-storage    "prk"
-                                         :office-commercial  "off"
-                                         :real-estate        "rea"}
-                        :housing-types {:any-housing 0
-                                        :apartment 1
-                                        :condo 2
-                                        :cottage-cabin 3
-                                        :duplex 4
-                                        :flat 5
-                                        :house 6
-                                        :in-law 7
-                                        :loft 8
-                                        :townhouse 9
-                                        :manufactured 10
-                                        :assisted-living 11
-                                        :land 12}}}
-             (find-category "housing"))))
-                                        
-
-
+  (it "returns a map of housing category attributes"
+    (should= housing (find-category "housing"))))
--- src/clj/sherlac/query_craig/category.clj
+++ src/clj/sherlac/query_craig/category.clj
@@ -1,24 +1,43 @@
 (ns sherlac.query-craig.category)
 
+(def housing
+  {:sub-categories {:default            "hhh"
+                    :apartments-housing "apa"
+                    :rooms-shared       "roo"
+                    :sublets-temporary  "sub"
+                    :housing-wanted     "hsw"
+                    :vacation-rentals   "vac"
+                    :parking-storage    "prk"
+                    :office-commercial  "off"
+                    :real-estate        "rea"}
+
+   :housing-types  {:any-housing         0
+                    :apartment           1
+                    :condo               2
+                    :cottage-cabin       3
+                    :duplex              4
+                    :flat                5
+                    :house               6
+                    :in-law              7
+                    :loft                8
+                    :townhouse           9
+                    :manufactured       10
+                    :assisted-living    11
+                    :land               12}
+
+   :search-options {"zoomToPosting"     "zoomToPosting="
+                    "sub-category"      "catAbb="
+                    "query"             "query="
+                    "min-rent"          "minAsk="
+                    "max-rent"          "maxAsk="
+                    "bedrooms"          "bedrooms="
+                    "housing-type"      "housing_type="
+                    "excats"            "excats="
+                    "picture"           "hasPic="
+                    "title"             "srchType="}})
+
+(def all-categories
+  {"housing" housing})
+
 (defn find-category [category]
-   {:housing {:sub-categories {:apartments-housing "apa"
-                               :rooms-shared       "roo"
-                               :sublets-temporary  "sub"
-                               :housing-wanted     "hsw"
-                               :vacation-rentals   "vac"
-                               :parking-storage    "prk"
-                               :office-commercial  "off"
-                               :real-estate        "rea"}
-              :housing-types {:any-housing 0
-                              :apartment 1
-                              :condo 2
-                              :cottage-cabin 3
-                              :duplex 4
-                              :flat 5
-                              :house 6
-                              :in-law 7
-                              :loft 8
-                              :townhouse 9
-                              :manufactured 10
-                              :assisted-living 11
-                              :land 12}}})
+  (get all-categories category))
